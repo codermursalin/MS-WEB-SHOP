50cb38496d2f13e0af673dfa1a4150001afaf88f
Mursalin Sk <codermursalin>
2023-01-06 05:41:35 +0530

    change Deploy on render

--- server/server.js
+++ server/server.js
@@ -20,11 +20,10 @@ app.use('/api/orders/',orderRouter);
 app.use('/api/upload',uploadRoute);
 console.log(NODE_ENV);
 console.log(NODE_ENV === 'production');
-
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('./client/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve('./client', 'build','index.html')));
   }
 app.use(errorHandler);
 
