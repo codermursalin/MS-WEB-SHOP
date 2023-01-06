e58c2a4f96258957a77ec3f5dffe6a54c4360589
Mursalin Sk <codermursalin>
2023-01-06 05:49:18 +0530

    change Deploy on render

--- server/server.js
+++ server/server.js
@@ -23,7 +23,8 @@ console.log(NODE_ENV === 'production');
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static('./client/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve('./client', 'build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.join('./client', 'build','index.html')));
+    console.log(path.join('./client', 'build','index.html'))
   }
 app.use(errorHandler);
 
